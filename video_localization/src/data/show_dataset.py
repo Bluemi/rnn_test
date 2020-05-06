acc1f5f9cc3aef0ff1f681f73230dd69c76f2327
Bluemi <bruno.schilling@protonmail.ch>
2020-05-06 16:33:09 +0200

    show dataset now prints num of frames

--- video_localization/src/data/show_dataset.py
+++ video_localization/src/data/show_dataset.py
@@ -1,9 +1,12 @@
-from data.data import chose_dataset_placeholder_from_database
-from util.show_frames import show_frames, ZoomRenderer
+from data.data import DatasetPlaceholder, chose_dataset_placeholder, DataInfo
+from util.show_frames import show_frames
 
 
 def show_dataset(args):
-    dataset_placeholder = chose_dataset_placeholder_from_database(args.database_directory)
+    placeholders = DatasetPlaceholder.list_database(args.database_directory)
+    joined_dataset_infos = DataInfo.join(map(lambda ds_placeholder: ds_placeholder.data_info, placeholders))
+    print('num samples: {}'.format(joined_dataset_infos.num_samples))
+    dataset_placeholder = chose_dataset_placeholder(placeholders)
 
     dataset = dataset_placeholder.load()
 
