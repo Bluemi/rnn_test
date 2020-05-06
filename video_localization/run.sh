7fe4356e09c063f0d330900d9177d73a1f85fc74
Bluemi <bruno.schilling@protonmail.ch>
2020-05-06 15:20:18 +0200

    updated run.sh

--- video_localization/run.sh
+++ video_localization/run.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-database='res/right_eye'
+database='res/right_hand'
 
-# python3 ./src/main.py test-camera
-# python3 ./src/main.py create-video-dataset $database
-# python3 ./src/main.py annotate-dataset $database
-# python3 ./src/main.py show-dataset $database
-# python3 ./src/main.py train-conv-model $database
+if [ "$1" == "test" ]; then
+	python3 ./src/main.py test-camera
+elif [ "$1" == "show" ]; then
+	python3 ./src/main.py show-dataset $database
+elif [ "$1" == "create" ]; then
+	python3 ./src/main.py create-video-dataset $database
+elif [ "$1" == "anno" ]; then
+	python3 ./src/main.py annotate-dataset $database
+elif [ "$1" == "train" ]; then
+	python3 ./src/main.py train-conv-model $database --show
+elif [ "$1" == "import" ]; then
+	python3 ./src/main.py import-video $database "$2"
+fi
